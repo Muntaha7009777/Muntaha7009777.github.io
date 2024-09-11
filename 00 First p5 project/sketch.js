bce89d4d890c4fcee3aa7bbf9284391fde8a0951
Muntaha7009777 <7009777@spsd.sk.ca>
2024-09-11 11:14:16 -0600

    Made first p5 project
    Made some changes to p5 project and getting familiar with it

---
+++ 00 First p5 project/sketch.js
@@ -0,0 +1,19 @@
+// First Day Demo
+// Muntaha Chowdhury
+// Sept 10, 2024
+//
+// Purpose of the code
+
+
+// Interactive Programs -> don't finish...
+
+function setup() {
+  //Happens one time, at the start
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  //Happens over and over, targeting 60fps
+  // background(220);
+  circle(mouseX, mouseY, 20)
+}
