7b8334d9ac4b7bd63b01842855c406bfe1f8ae38
Muntaha7009777 <7009777@spsd.sk.ca>
2024-09-14 22:45:29 -0600

    Continued Challenge: 00 Drawing Alien
    - Fixed scalability
    - Added button for allowing scalability
    - Added movement using keyboard + mouse
    
    (Done outside of class)

--- Challenges/00 Drawing Alien/sketch.js
+++ Challenges/00 Drawing Alien/sketch.js
@@ -4,81 +4,110 @@
 // Use Shapes to draw an alien that is scalable and movable
 
 
-
-/*
-
-Plan:
-
-- introduce variables
-  → x (reference point)
-  → radius (also used as rectangle height and doubled to be rectangle width)  [!!!ask through browser]
-  → moveX, moveY
-
-- create shapes
-  → circle
-  → rectangle
-  → 2x rectangle (legs)
-  → eyes
-  → mouth
-
-- introduce scalability
-  → function that changes size [!!!add a button]
-
-- functions for moving
-  → keyboard and mouse
-
-*/
-
-
+//----Variables for interactions
 let radius = 100;                      // main reference length for scalability
+let refX, refY;                        // reference point
+let moveX = 5, moveY = 5;            // movement
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  refX = width / 2;
+  refY = height / 2;
 }
-
-//----Variables for interactions
-let refX = width/2, refY = height/2;  // reference point
-let moveX = 10, moveY = 10;           // movement
 
 
 function draw() {
   background(220);
+  instructions();
   drawShape();
-  // moveAlien();
+  moveAlien();  // keyPressed() wasn't used to allow smoother movement
 }
+
+
+
+function instructions() {
+  textSize(10);
+  text('Use Keyboard/mouse to move', 20, 20);
+
+  let scaler = createButton('Change alien size');
+  scaler.style('font-size', '10px');
+  scaler.style('color', color(0, 120, 200));
+  scaler.position(20, 30);
+  scaler.mousePressed(changeSize);
+}
+
+function changeSize() {
+  radius = prompt('Enter a number');
+  print(radius);
+}
+
 
 
 function drawShape() {
   // draws alien
 
-  //---body
+  //body----------------------------------------------------------
   fill(170, 255, 170);
   noStroke();
 
   // legs
-  rect((width/2)-50, height/2, 10, 75);   //my left
-  rect((width/2)+50, height/2, -10, 75);  //my right
+  rect(refX - (radius / 2), refY, radius / 10, radius * 3 / 4);   //my left
+  rect(refX + (radius / 2), refY, -radius / 10, radius * 3 / 4);  //my right
 
   // body
-  rect((width/2)-50, height/2, 100, 100/2);
+  rect(refX - (radius / 2), refY, radius, radius / 2);
 
   // head (circle)
-  circle(width/2, height/2, radius);
+  circle(refX, refY, radius);
 
-  //---facial features
+
+  //facial features-----------------------------------------------
   fill(0);
 
   // eyes
-  circle((width/2)-25, height/2, 10);   //my left
-  circle((width/2)+25, height/2, 10);   //my right
+  circle(refX - (radius / 4), refY, radius / 10);   //my left
+  circle(refX + (radius / 4), refY, radius / 10);   //my right
 
   // mouth
-  rect((width/2), (height/2)+25, 20, 5, 5);
+  rect(refX, (refY) + (radius / 4), radius / 4, radius / 20, 5);
 }
 
 
 
-// function moveAlien() {
+function moveAlien() {
+  // function for moving alien by mouse and keyboard
 
-// }
+  //KeyBoard----------------------------------------------------
+  if (keyIsPressed) {
+    // X-axis
+    if (keyCode === LEFT_ARROW) {
+      refX -= moveX;
+    } else if (keyCode === RIGHT_ARROW) {
+      refX += moveX;
+    }
 
+    // Y-axis
+    if (keyCode === UP_ARROW) {
+      refY -= moveY;
+    } else if (keyCode === DOWN_ARROW) {
+      refY += moveY;
+    }
+  }
+
+
+  //Mouse----------------------------------------------------
+  if (mouseIsPressed) {
+    let distanceX = mouseX - refX;
+    let distanceY = mouseY - refY;
+    refX += distanceX;
+    refY += distanceY;
+  }
+
+
+  //Fix position-------------------------------------------
+  if (refX > width) refX = 0;
+  if (refX < 0) refX = width;
+  if (refY > height) refY = 0;
+  if (refY < 0) refY = height;
+}
