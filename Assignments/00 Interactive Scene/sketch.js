a46e9f6da58159a68dc469148392283550a4a0ea
Muntaha7009777 <7009777@spsd.sk.ca>
2024-09-17 12:16:22 -0600

    Updated Assignment: 00 Interactive Scene
    - Added sun
    - Working on Stars
    - Put a placeholder for water

--- Assignments/00 Interactive Scene/sketch.js
+++ Assignments/00 Interactive Scene/sketch.js
@@ -5,6 +5,7 @@
 
 
 let currentBack = 1;
+let currentSky = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,16 +13,18 @@ function setup() {
 
 function draw() {
   background(220); //fallback bg
-  setBg();
+  setGardenBg();
+  setSkyBg();
+  setWaterBg();
   displayFlowerCounter();
 
 
   text('Muntaha Chowdhury', width-120, height-10);
-  text((mouseX + ', ' + mouseY), mouseX, mouseY);
+  fill(0); text((mouseX + ', ' + mouseY), mouseX, mouseY);
 }
 
 
-function setBg() {
+function setGardenBg() {
   // Sets background of garden according to currentBack
 
   if (currentBack === 0) {
@@ -38,15 +41,73 @@ function setBg() {
   }
 }
 
-
-function displayFlowerCounter() {
-  // display + keeep track --- of the 4 flowers on the top left 
-  fill(236, 247, 27);
-  circle(width/2, height/2, 40);
-  triangle(330, 459, 364, 459, 345, 428);
+function setWaterBg() {
+  fill(220);
+  rect(width/20, height/5, 150, 150, 4);
 }
 
 
+function setSkyBg() {
+  noStroke();
+
+  if (currentSky === 0)  {
+    fill(199, 247, 255);
+    rect(0, 0, width, height/4);
+    fill(255, 203, 0);
+    circle(width/2, 0, 200);
+  }
+  else  {
+    fill(82, 104, 130);
+    rect(0, 0, width, height/4);
+    fill(255);
+    circle(width/2, 0, 200);
+  }
+}
+
+
+
+
+function displayFlowerCounter() {
+  // display + keeep track --- of the 4 flowers on the top left 
+
+  stroke(0);
+  fill(150, 147, 255);
+
+  // Petals
+  rect((width/2)-25, (height/2)-25, 50)
+  // triangle(320, 460, 370, 460, 344, 410);
+  // triangle(360, 450, 360, 500, 410, 473);
+  // triangle(370, 479, 330, 500, 375, 525);
+  // triangle(335, 450, 335, 500, 290, 470);  
+  // triangle(330, 463, 350, 500, 310, 525);  
+
+
+  // center
+  fill(236, 247, 27);
+  circle(width/2, height/2, 40);
+
+}
+
+function mousePressed() {
+
+  // Sun
+  if ( mouseY <= 100 && (mouseX >= (width/2)-100 && mouseY <= (width/2)+100)) {
+    if (currentSky === 1) currentSky = 0;
+    else {
+      currentSky = 1;
+      let starsX = [];
+      let starsY = [];
+      for (let i = 0; i < 20; i++) {
+        let x = random(0, width), y = random(0, height/4);
+        starsX.push(x); starsY.push(y);
+      }
+      for (let j = 0; j < starsX.length; j++) {
+        fill(255);
+        circle(starsX[j], starsY[j], 20);
+      }
+    };
+  }
+}
 
 /*
 
@@ -76,5 +137,18 @@ Plan
   
   → Background
     ► Middle button changes flower garden
+
+
+
+
+
+Order of work (high-level):
+
+- Make flowers
+- Make background
+- Do Sun first
+- Do Water
+- Design Protagonist
+- Work with protaonist
 
 */
