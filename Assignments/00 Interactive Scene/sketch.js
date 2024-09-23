187982f0905cab11cac13850852ffd2d0bc54f73
Muntaha7009777 <7009777@spsd.sk.ca>
2024-09-22 23:22:21 -0600

    Updated Assignment: 00 Interactive Scene
    - designed 2 more backgrounds

--- Assignments/00 Interactive Scene/sketch.js
+++ Assignments/00 Interactive Scene/sketch.js
@@ -4,27 +4,29 @@
 // An interactive scene which can be controlled by both mouse and keyboard
 
 
-let currentBack = 1;
+let currentBack = 3;
 let currentSky = 0;
 let flowersCollected = 0;
-let charX = width/2;
-let charY = height/2;
+let charX;
+let charY;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  charX = width/2;
+  charY = height/2;
 }
 
 function draw() {
   background(220); //fallback bg
-  setGardenBg();
   setSkyBg();
   setSkylineBg();
-  // setWaterBg();
-  setFlowerCounter();
+  setGardenBg();
+  // setFlowerCounter();
   // flowerPopup();
-  displayFlower();
+  // displayFlower();
 
   noStroke();
+  fill(0);
   textSize(12);
   text('Muntaha Chowdhury', width-120, height-10);
   fill(0); text((mouseX + ', ' + mouseY), mouseX, mouseY);
@@ -35,55 +37,23 @@ function setGardenBg() {
   // Sets background of garden according to currentBack
 
   if (currentBack === 0) {
-    background(152, 242, 136);
+    fill(152, 242, 136);
+    rect(0, height, width, -(height)*2/3)
   }
   if (currentBack === 1) {
-    background(182, 147, 255);
+    fill(182, 147, 255);
+    rect(0, height, width, -(height)*2/3)
   }
   if (currentBack === 2) {
-    background(1255, 237, 120);
+    fill(255, 237, 120);
+    rect(0, height, width, -(height)*2/3)
   }
   if (currentBack === 3) {
-    background(250, 250, 250);
+    fill(250, 250, 250);
+    rect(0, height, width, -(height)*2/3)
   }
 }
 
-function setWaterBg() {
-
-  //body
-  fill(220);
-  rect(20, height/3, 150, 98, 4);
-
-  //slab
-  fill(50);
-  ellipse(95, (height/3)+3, 154, 35);
-  fill(180);
-  ellipse(95, height/3, 154, 30);
-  fill(36, 39, 64);
-  ellipse(95, (height/3)+2, 100, 15);
-
-  // sticks
-  fill(64, 49, 36);
-  rect(50, height/4, 5, 80);
-  rect(130, height/4, 5, 80);
-
-  //head
-  rect(40, (height/4)-20, 110, 30);
-  //         top point       |   bottom point   |  outermost point
-  triangle(40, (height/4)-20, 40, (height/4)+10, 20, (height/4)+10);
-  triangle(150, (height/4)-20, 150, (height/4)+10, 170, (height/4)+10);
-
-  // rope
-  fill(181, 171, 163);
-  rect(70, (height/4)+20, 5, 67);
-
-  // mid
-  fill(64, 49, 36);
-  rect(50, (height/4)+20, 80, 20);
-  rect(130, (height/4)+25, 25, 5);
-  rect(150, (height/4)+25, 5, 25);
-  rect(150, (height/4)+50, 25, 5);
-}
 
 function setSkyBg() {
   noStroke();
@@ -103,18 +73,92 @@ function setSkyBg() {
 }
 
 function setSkylineBg() {
-  randomSeed(5);
+  randomSeed(8);
   
-  for (let i = 0; i < width; i+=50) {
-    fill(40, 57, 74);
-    let buidingHeight = random(40, 130);
-    rect(i, height/3, 50, -buidingHeight);
+  // Green - mountains
+  if (currentBack === 0) {
+    fill(72, 90, 56);
+    ellipse((width-width/3), height/3, width, height/2);
+
+    fill(72, 130, 56);
+    ellipse(50, height/3, width, height/2.5);
+    
+    fill(72, 110, 56);
+    ellipse(width-50, height/2.5, width, height/2.5);
+
+
+    for (let tree = 0; tree < 40; tree++) {
+      let treeX = random(0, width);
+      let treeY = random (height/3, height/6.5);
+      fill(255-(tree*2), 184, 245);
+      triangle(treeX-6, treeY, treeX+6, treeY, treeX, treeY-20);
+      fill(0);
+      rect(treeX-1, treeY, 2, 5);
+      console.log(treeX, treeY);
+    }
+
   }
 
-  for (let i = 0; i < width; i+=50) {
-    let buidingHeight = random(20, 100);
-    fill(56, 81, 105);
-    rect(i-5, height/3, 50, -buidingHeight);
+  // Purple - city
+  else if (currentBack === 1) {
+    for (let i = 0; i < width; i+=50) {
+      fill(40, 57, 74);
+      let buidingHeight = random(40, 130);
+      rect(i, height/3, 50, -buidingHeight);
+    }
+
+    for (let i = 0; i < width; i+=50) {
+      let buidingHeight = random(20, 100);
+      fill(56, 81, 105);
+      rect(i-5, height/3, 50, -buidingHeight);
+    }
+  }
+
+  // yellow - desert
+  else if (currentBack === 2) {
+
+    //pattern:       (bottom right)      (bottom left)               (top)
+
+    fill(237, 206, 114);
+    triangle(width/2, height/3,  -40, height/3,         width/6, height/8);     
+    for (let cactus = 0; cactus < 5; cactus++) {
+      let cactusX = random(width/20, width/2.5);
+      let cactusY = random(height/3.5, height/5);
+      fill(0, 255, 0);
+
+      rect(cactusX, cactusY, 5, 25, 4, 4, 0, 0);
+      rect(cactusX-8, cactusY+5, 20, 5, 4);
+      rect(cactusX-8, cactusY-10, 5, 20, 4)
+      rect(cactusX+8, cactusY, 5, 10, 4)
+    }
+
+    fill(194, 148, 50);
+    triangle(width+20, height/3, (width/2)-20, height/3,  (width-width/6), height/5); 
+    for (let cactus = 0; cactus < 5; cactus++) {
+      let cactusX = random(width-width/2, width-width/8);
+      let cactusY = random(height/3, height/5);
+      fill(0, 255, 0);
+
+      rect(cactusX, cactusY, 5, 25, 4, 4, 0, 0);
+      rect(cactusX-8, cactusY+5, 20, 5, 4);
+      rect(cactusX-8, cactusY-10, 5, 20, 4)
+      rect(cactusX+8, cactusY, 5, 10, 4)
+    }
+
+  }
+
+  // white - snow
+  else {
+
+    // Igloo
+    fill(220);
+    circle(width/4, height/3, width/3);
+    fill(230);
+    rect(width/4, height/3, 70, -height/10, 0, 0, 100, 100);
+    fill(220);
+    rect(width/3.75, height/3, 50, -height/12, 0, 0, 100, 100);
+
+    
   }
 }
 
@@ -159,8 +203,8 @@ function displayFlower(xPos, yPos) {
 function mousePressed() {
 
   // change currentBack
-  if (mouseButton === CENTER) {
-    if (currentBack === 3) currentBack = 1;
+  if (mouseButton === LEFT) {
+    if (currentBack === 3) currentBack = 0;
     else currentBack += 1;
   }
 
@@ -169,16 +213,16 @@ function mousePressed() {
     if (currentSky === 1) currentSky = 0;
     else {
       currentSky = 1;
-      let starsX = [];
-      let starsY = [];
-      for (let i = 0; i < 20; i++) {
-        let x = random(0, width), y = random(0, height/4);
-        starsX.push(x); starsY.push(y);
-      }
-      for (let j = 0; j < starsX.length; j++) {
-        fill(255);
-        circle(starsX[j], starsY[j], 20);
-      }
+      // let starsX = [];
+      // let starsY = [];
+      // for (let i = 0; i < 20; i++) {
+      //   let x = random(0, width), y = random(0, height/4);
+      //   starsX.push(x); starsY.push(y);
+      // }
+      // for (let j = 0; j < starsX.length; j++) {
+      //   fill(255);
+      //   circle(starsX[j], starsY[j], 20);
+      // }
     };
   }
 
@@ -200,7 +244,12 @@ Plan
   → Sun
     ► Can be turned to moon (slows Time variable for water)
 
-  → Background
-    ► Middle button changes flower garden
+  → Backgrounds
+    ► Design
 
+
+- Currently in progress
+  → Design Backgrounds
+  → Design Character
+  → Make character movable
 */
