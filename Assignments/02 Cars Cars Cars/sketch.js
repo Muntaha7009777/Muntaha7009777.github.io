d2b7ecca9034197a07774a744a6d1883578e793d
Muntaha7009777 <7009777@spsd.sk.ca>
2024-10-25 12:16:07 -0600

    12 Array Demos 2 + Updated Assignment 2
    - refactored code
    - added speed sliders
    - added instructions
    
    - I don't have Array Demos 1 ;-;

--- Assignments/02 Cars Cars Cars/sketch.js
+++ Assignments/02 Cars Cars Cars/sketch.js
@@ -28,6 +28,7 @@ function setup() {
   rectMode(CENTER);
   counter = 0;
   trafficLight = new TrafficLight();
+  drawSpeedSliders();
 
   // --cars
   for (let i = 0; i < 10; i++) {
@@ -41,10 +42,10 @@ function setup() {
 
 function draw() {
   // main function manager
-
   background(220);
-  trafficLight.action();
   drawRoad();
+  infoNspeed();
+  trafficLight.action();
   
   for (let i = 0; i < eastbound.length; i++) {
     eastbound[i].action();
@@ -59,10 +60,13 @@ function draw() {
 
 
 function drawRoad() {
-  // draw road with line
+  // draw road with yellow separating line
+
+  // the road
   fill(0);
   rect(width/2, height/2, width, height*(1/2));
 
+  // the dashed line
   fill('yellow');
   noStroke();
   for (let i = 0; i < width ; i+=25) {
@@ -71,12 +75,14 @@ function drawRoad() {
 }
 
 function drawVehicle(choice, x, y, color, dir) {
-  // draw 0-car 1-truck
+  // draw a vehicle :  0-car 1-truck
 
   if (choice === 0) {           //car
-
+    //body
     fill(color);
-    rect(x, y, carSize+carSize/2, carSize, 6);
+    rect(x, y, carSize+carSize/2, carSize, 6);  
+
+    // windows
     fill(0);
     rect(x+carSize/2, y, carSize-carSize/1.5, carSize-carSize/2.5, 2, 10, 10, 2);      //front window
     rect(x-carSize/2, y, carSize-carSize/1.5, carSize-carSize/2.5, 10, 2, 2, 10);   //back window
@@ -97,38 +103,53 @@ function drawVehicle(choice, x, y, color, dir) {
     fill(color);
     rect(x, y, truckSize+truckSize/2, truckSize+truckSize/10, 2);
 
+    // windows
     fill(0);
     if (dir === 'east') {
-      rect(x+truckSize*1.25, y, truckSize-truckSize/1.5, truckSize-truckSize/2.5, 2, 10, 10, 2);      //front window
-      arc(x+truckSize*1.05, y-truckSize/2.5, truckSize/2, 10, 0, PI);                                 //left window
-      arc(x+truckSize*1.05, y+truckSize/2.5, truckSize/2, 10, PI, 0);                                 //right window
+      rect(x+truckSize*1.25, y, truckSize-truckSize/1.5, truckSize-truckSize/2.5, 2, 10, 10, 2);      //front 
+      arc(x+truckSize*1.05, y-truckSize/2.5, truckSize/2, 10, 0, PI);                                 //left 
+      arc(x+truckSize*1.05, y+truckSize/2.5, truckSize/2, 10, PI, 0);                                 //right 
     }
     else {
-      rect(x+truckSize*-1.25, y, truckSize-truckSize/1.5, truckSize-truckSize/2.5, 2, 10, 10, 2);      //front window
-      arc(x+truckSize*-1.05, y-truckSize/2.5, truckSize/2, 10, 0, PI);                                 //left window
-      arc(x+truckSize*-1.05, y+truckSize/2.5, truckSize/2, 10, PI, 0);
+      rect(x+truckSize*-1.25, y, truckSize-truckSize/1.5, truckSize-truckSize/2.5, 2, 10, 10, 2);      //front 
+      arc(x+truckSize*-1.05, y-truckSize/2.5, truckSize/2, 10, 0, PI);                                 //left 
+      arc(x+truckSize*-1.05, y+truckSize/2.5, truckSize/2, 10, PI, 0);                                 //right 
     }
   }
 }
 
 function drawTrafficLight(light) {
+  // draw the traffic light + only the current light has a color
   fill(0);
   rect(width/2, 0, height/10, height/2.25);
 
-
-  if (light === 'red') fill('red');
+  // the lights
+  if (light === 'red') fill(light);
   else fill(100);
   circle(width/2, height/20, height/20);
 
-  if (light === 'yellow') fill('yellow');
+  if (light === 'yellow') fill(light);
   else fill(100);
   circle(width/2, height/9, height/20);
 
-  if (light === 'green') fill('green');
+  if (light === 'green') fill(light);
   else fill(100);
   circle(width/2, height/5.75, height/20);
 }
 
+function drawSpeedSliders() {
+  // sliders for setting speed ( at top left)
+
+  // max
+  maxSlider = createSlider(20, 50, 20);
+  maxSlider.position(10, 10);
+  maxSlider.size(90);
+  // min
+
+  minSlider = createSlider(2, 10, 2);
+  minSlider.position(10, 40);
+  minSlider.size(90);
+}
 
 
 
@@ -136,7 +157,7 @@ function drawTrafficLight(light) {
 
 
 class Vehicle {
-  // make a vehicle
+  // make a vehicle class that: displats, moves, changes speed, and color changing
 
   constructor(type, dir) {
     this.type = type;
@@ -148,6 +169,7 @@ class Vehicle {
       this.xSpeed = random(minSpeed, maxSpeed);
       this.color = color(50, 10, 100);
 
+      // lane determiner
       if (lane === 0) this.y = height/2 + truckSize;
       else if (lane === 1) this.y = height/2 + height/8;
       else this.y = height*3/4 - truckSize;
@@ -156,8 +178,9 @@ class Vehicle {
     else if (this.dir === 'west') {
       this.x = width;
       this.xSpeed = random(-maxSpeed, -minSpeed);
-      this.color = color(50, 200, 100);
-
+      this.color = color(50, 10, 100);
+      
+      // lane determiner
       if (lane === 0) this.y = height/4 + truckSize;
       else if (lane === 1) this.y = height/2 - height/8;
       else this.y = height/2 - truckSize;
@@ -166,6 +189,7 @@ class Vehicle {
   }
 
   display() {
+    // display the cars
     if (this.type === 0) drawVehicle(0, this.x, this.y, this.color, this.dir);
     else if (this.type === 1) drawVehicle(1, this.x, this.y, this.color, this.dir);
   }
@@ -267,6 +291,7 @@ class TrafficLight {
 
 
 function mouseClicked() {
+  // add more cars on mouse click (left click: east  // left+shift: west)
   if (mouseButton === LEFT && keyCode === SHIFT) {
     eastbound.push(new Vehicle(round(random(0,1)), 'east'));
   }
@@ -276,7 +301,32 @@ function mouseClicked() {
 }
 
 function keyPressed() {
+  // turn traffic light red with spacebar
   if (keyCode === 32) {
     counter = 160;
   }
 }
+
+function infoNspeed() {
+  // display instructions (car adding), change speed
+
+  // speed setters and displayers
+  maxSpeed = maxSlider.value();
+  minSpeed = minSlider.value();
+  fill(0);
+  text((maxSpeed + '  (Max speed)'), 110, 25);
+  text((minSpeed + '  (Min speed)'), 110, 55) ;
+
+  // The instructions
+  fill('lightblue');
+  rect(20, 75, 20, 20, 4);
+  fill(0);
+  text('?', 17, 80);
+  text('Left click to add more westbound cars', 10, 100);
+  text('Left click + shift to add more eastbound cars', 10, 120);
+  text('Spacebar to turn traffic light red', 10, 140);
+}
+
+
+
+
