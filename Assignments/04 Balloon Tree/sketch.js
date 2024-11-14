7062856be86cb36a4b7eba591daae4705c02b733
Muntaha7009777 <7009777@spsd.sk.ca>
2024-11-14 12:11:43 -0600

    Assignment 04: Balloon Tree
    - finished

--- Assignments/04 Balloon Tree/sketch.js
+++ Assignments/04 Balloon Tree/sketch.js
@@ -1,26 +1,32 @@
 // Balloon tree
 // Muntaha Chowdhury
 // 13 November, 2024
-// ADD STATEMENT HERE
+// Ballon tree that changes based on keypresses and mouse movements
 /* eslint-disable no-extra-parens */
 
 
 let scale = 20;
+let noLeavesDepth = 5;
+let depthStart = 6;
+let seed;
 
 function setup() {
-  createCanvas(500, 500);
-  background(255);
+  createCanvas(windowWidth, windowHeight);
+  seed = random(60);
 }
 
 function draw() {
-  randomSeed(58);
+  background(255);
+  randomSeed(seed);
   // drawTree(width / 2, height*0.9, 90, 6);
-  drawTree(width / 2, height, 90, 6);
+  drawTree(width / 2, height, 90, depthStart);
 }
 
 function drawLine(x1, y1, x2, y2, depth) {
   //draw a line segment connecting (x1,y1) to (x2,y2)
+  strokeWeight(depth);
   line(x1, y1, x2, y2);
+  strokeWeight(1);
 }
 
 function drawTree(x1, y1, angle, depth) {
@@ -28,17 +34,35 @@ function drawTree(x1, y1, angle, depth) {
     let x2 = x1 + (cos(radians(angle)) * depth * scale); //calculate endpoints of current branch
     let y2 = y1 - (sin(radians(angle)) * depth * scale); //using trig ratios. Get shorter based on depth
     drawLine(x1, y1, x2, y2, depth);
-    drawLeaf(x2, y2, scale*2);
-
-
+    
+    let controlAngle = map(mouseX, 0, width, 10, 60);
     //for a 3-branch tree:
-    drawTree(x2, y2, angle - 24, depth - 1);
-    drawTree(x2, y2, angle      ,depth - 1);
-    drawTree(x2, y2, angle + 24, depth - 1);
+    drawTree(x2, y2, angle - controlAngle, depth - 1);
+    drawTree(x2, y2, angle               , depth - 1);
+    drawTree(x2, y2, angle + controlAngle, depth - 1);
+    
+    drawLeaf(x2, y2, depth);
   }
 }
 
 
-function drawLeaf(x, y, d) {
-  circle(x, y, d);
+function drawLeaf(x, y, depth) {
+  if (depth < noLeavesDepth ) {
+    fill(random(255), random(255), random(255));
+    let size = (random(10, 12));
+    let gradient = drawingContext.createRadialGradient(x, y, size/3, x, y, size);
+    fill(gradient);
+    circle(x, y, size*depth);
+  }
+  // implicit base
+}
+
+
+function keyPressed() {
+  if (keyCode === 90 && noLeavesDepth !== 1) {
+    noLeavesDepth--;
+  }
+  if (keyCode === 88 && noLeavesDepth !== depthStart+1) {
+    noLeavesDepth++;
+  }
 }
