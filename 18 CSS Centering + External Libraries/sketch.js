8158ef4816feff9c673c1970082db877b2db81ee
Muntaha7009777 <7009777@spsd.sk.ca>
2024-11-27 11:28:33 -0600

    18 Continued
    - added sound to the bouncing ball

--- 18 CSS Centering + External Libraries/sketch.js
+++ 18 CSS Centering + External Libraries/sketch.js
@@ -2,9 +2,15 @@
 // Muntaha Chowdhury
 // 22 Nov, 2024
 
+let music, bounceSound;
 let started = false;
 let pos; let vel;
 let totalBounces = 0;
+
+function preload(){
+  music = loadSound("assets/background.mp3");
+  bounceSound = loadSound("assets/bounceSound.wav");
+}
 
 function setup() {
   createCanvas(300, 200);
@@ -28,6 +34,8 @@ function draw() {
     text("Click to begin", width/2, height/2);
     if (mouseIsPressed) {
       started = true;
+      music.setVolume(0.1); //10%
+      music.loop();
     }
   }
   else {
@@ -38,13 +46,16 @@ function draw() {
 
 function updateBall() {
   pos.add(vel);
+  bounceSound.setVolume(0.9);
   if (pos.x < 0 || pos.x > width) {
     totalBounces++;
+    bounceSound.play();
     vel.x *= -1;
     localStorage.setItem("bounce", totalBounces);
   }
   if (pos.y < 0 || pos.y > height) {
     totalBounces++;
+    bounceSound.play();
     vel.y *= -1;
     localStorage.setItem("bounce", totalBounces);
   }
