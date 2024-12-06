982cf3ebd6bddb29094cad41f068aaed2f83499a
Muntaha7009777 <7009777@spsd.sk.ca>
2024-12-06 12:17:01 -0600

    20 Multiple Files and Inheritance

---
+++ 20 Multiple Files and Inheritance/sketch.js
@@ -0,0 +1,23 @@
+// Multiple Files and Inheritance
+// Muntaha Chowdhury
+// Dec 6th, 2024
+// 
+let objects = [];
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for(let i = 0; i<10; i++){
+    objects.push(new AnimatedObject(random(width), random(height)));
+    objects.push(new LineObject());
+    objects.push(new CircleObject(random(width), random(height)));
+  }
+}
+
+function draw() {
+  background(220);
+  for(let o of objects){
+    o.move();
+    o.display();
+  }
+}
